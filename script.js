dbdde7d4b8764af51b3d189b36e78456f406e99e
Carlos Brandsma Nainggolan <carlosnainggolan@gmail.com>
2026-06-08 10:32:39 +0700

    refactor: making all script to script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let password = document.getElementById("password");
+let toggler = document.getElementById("toggle-password");
+
+toggler.addEventListener("click", hideShowPassword);
+
+function hideShowPassword() {
+    if (password.type === "password") {
+        password.setAttribute("type", "text");
+        toggler.classList.remove("bi-eye-slash");
+        toggler.classList.add("bi-eye");
+    } else {
+        password.setAttribute("type", "password");
+        toggler.classList.remove("bi-eye");
+        toggler.classList.add("bi-eye-slash");
+    }
+}
